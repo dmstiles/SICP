136d04d242a7d59425f00b2042ff7d815e25c50a
Drew Michael Stiles <drewmstiles@gmail.com>
2016-06-18 14:27:56 -0700

    Finishes exercise 1.24

--- ex1-24.scm
+++ ex1-24.scm
@@ -29,7 +29,7 @@
 
 
 (define (fast-prime? n times)
-  (cond ((= n 0) true)
+  (cond ((= times 0) true)
 	((fermat-test n) (fast-prime? n (- times 1)))
 	(else false)))
 ;Value: fast-prime?
@@ -43,7 +43,7 @@
 
 
 (define (start-prime-test n start-time)
-  (if (fast-prime? n)
+  (if (fast-prime? n 100)
       (report-prime (- (runtime) start-time))))
 ;Value: start-prime-test
 
@@ -78,6 +78,18 @@
 ;Value: 1
 
 (fast-prime? 1000003 10)
+;Value: #t
+
+(search-for-primes 100 110)
+
+101 *** 0.
+103 *** 0.
+105 *** 0.
+107 *** 0.
+109 *** 0.
+Done
+;Unspecified return value 
+
 
 ;; Solution.
 
@@ -88,32 +100,59 @@
 
 (search-for-primes 10000000 10001000)
 ;; 10000019 => 0
-;; 10000079 => 0.01
+;; 10000079 => 0
 ;; 10000103 => 0
 
 (search-for-primes 100000000 100001000)
-;; 100000007 => 0.02
-;; 100000037 => 0.02
-;; 100000039 => 0.02
+;; 100000007 => 0
+;; 100000037 => 0
+;; 100000039 => 0
 
 (search-for-primes 1000000000 1000001000)
-;; 1000000007 => 0.06
-;; 1000000009 => 0.06
-;; 1000000021 => 0.06
+;; 1000000007 => 0
+;; 1000000009 => 0
+;; 1000000021 => 0
 
 (search-for-primes 10000000000 10000001000)
-;; 10000000019 => 0.2
-;; 10000000033 => 0.19
-;; 10000000061 => 0.19
+;; 10000000019 => 0
+;; 10000000033 => 0
+;; 10000000061 => 0
+
+;; Previous test entries are too small of magnitude for new algorithm
+;; with log(n) growth. Since the original implementation of the new Fermat
+;; probabilistic approach was being used with a repition factor of 1
+;; that repitition factor has now been increased to 100 in order to extend
+;; the execution time, as well as increase the confidence in the result.
+;; 
+;; The following tests use this new implementation with values doubling in
+;; magnitude for each subsequent test in order to observe the runtime growth
+;; for the Fermat probabilistic algorithm.
+
+;; 1e11
+(search-for-primes 100000000000 100000001000)
+;; 100000000003 *** 1.0000000000001563e-2
+;; 100000000019 *** 1.9999999999999574e-2
+;; 100000000057 *** 1.9999999999999574e-2
+
+;; 1e22
+(search-for-primes 10000000000000000000000 10000000000000000001000)
+;; 10000000000000000000009 *** 2.9999999999997584e-2
+;; 10000000000000000000057 *** 3.0000000000001137e-2
+;; 10000000000000000000081 *** 2.9999999999997584e-2
+
+;; 1e44
+(search-for-primes 100000000000000000000000000000000000000000000 100000000000000000000000000000000000000001000)
+;; 100000000000000000000000000000000000000000031 *** .07000000000000028
+;; 100000000000000000000000000000000000000000057 *** .07000000000000028
+;; 100000000000000000000000000000000000000000309 *** .05999999999999872
 
 
-;; Although the sequence of numbers tested above grew by a magnitude of 10 with
-;; each subsequent search, it can be seen that the time required to test such
-;; numbers only grew by an order of somewhere roughly around 3, or between 2 and 4 here. 
-;; Since the `prime?` procdure used in the primality test displays a sqrt(n) 
-;; order of growth the results obtained above seem to confirm the notion
-;; that the time required for programs run on a machine are proportional to the
-;; steps required in the computation. More plainly, since the number tested in each
-;; test grew by an order of 10, the order or growth for the `prime?` procedure
-;; should theoretically be sqrt(10), which is approximately 3.16, a value that
-;; corresponds directly to the observed runtime growth of the tested numbers.
+;; From the proceeding tests one can observe the algorithmic runtime growth
+;; for the `fast-prime?` procedure using the Fermat probabilistic method.
+;; As the order of magnitude doubled in each subsequent tests the runtime
+;; growth also doubled reflecting the predicted logrithmic order of growth.
+;; This shows that the size of the input must double its order of magnitude
+;; in order to double the time required to operate on this input. Such growth
+;; is a stark contrast to the previous sqrt(n) growth used in the `prime?`
+;; procedure where an input value 4 times as large was enough to double
+;; the runtime duration, since sqrt(4*n) => 2*sqrt(n)
