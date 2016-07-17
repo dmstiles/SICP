8c875f34c6de668563add32514c2933168835af9
drewmstiles <drewmstiles@gmail.com>
2016-07-17 11:19:42 -0700

    Finishes exercise 2.24

---
+++ ex-2-24.scm
@@ -0,0 +1,23 @@
+;; Exercise 2.24
+;;
+;; Description:
+;;
+;; Consider the following expression
+;;
+;;     (list 1 (list 2 (list 3 4)))
+;;
+;; Give the result printed by the interepter, the corresponding box and
+;; pointer structure, and the interpretation as a tree.
+
+
+
+;; Definitions:
+
+(list 1 (list 2 (list 3 4)))
+;Value 13: (1 (2 (3 4)))
+
+
+
+;; Solutions:
+;;
+;; Box and tree interpretations not shown.
