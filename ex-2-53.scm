b54e527c9b853ff5c65b86f8ace3ac010b15055b
drewmstiles <drewmstiles@gmail.com>
2017-01-06 15:26:43 -0800

    Finish exercise 2.53

---
+++ ex-2-53.scm
@@ -0,0 +1,31 @@
+;; Exercise 2.53
+;;
+;; Description:
+;;
+;; What does the interpreter print in response to evaluating the following expressions?
+
+
+
+;; Definitions:
+
+(list 'a 'b 'c)
+;Value 13: (a b c)
+
+(list (list 'george))
+;Value 14: ((george))
+
+(cdr '((x1 x2) (y1 y2)))
+;Value 15: ((y1 y2))
+
+(cadr '((x1 x2) (y1 y2)))
+;Value 16: (y1 y2)
+
+(pair? (car '(a short list)))
+;Value: #f
+
+(memq 'red '((red shoes) (blue socks)))
+;Value: #f
+
+(memq 'red '(red shoes blue socks))
+;Value 17: (red shoes blue socks)
+
