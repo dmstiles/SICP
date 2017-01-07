8299b5884aa75356c7d6a1c9f05489b901ef3c66
drewmstiles <drewmstiles@gmail.com>
2017-01-06 16:11:08 -0800

    Finish exercise 2.55

---
+++ ex-2-55.scm
@@ -0,0 +1,22 @@
+;; Exercise 2.55
+;;
+;; Description:
+;;
+;; Consider the following expression.
+;;
+;;    (car ''abracadabra)
+;;
+;; Why does the interpreter print `quote` upon evaluation?
+
+
+
+;; Definitions:
+(car ''abracadabra)
+;Value: quote
+
+;; Taken literally the `'` symbol evalautes to the procedure `quote` which tells the
+;; interpreter to regard the subsequent characters as symbols and not values.
+;; Something of the form 'a => (quote a), therfore the evaluation of ''abracadabra
+;; is interpreted as (quote (quote abracadabra)), or similarily, '(quote abracadabra).
+;; From this form it is evident that the interpreter sees a list of two symbols, quote
+;; and abracadabra, where quote is the `car` of that list.
